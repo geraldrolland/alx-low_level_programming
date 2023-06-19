f37a0a4990b42378f31204c4a8d594e0636fcc93
geraldrolland <geraldrolland123@gmail.com>
2023-06-19 12:26:47 -0700

    my task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,25 +12,19 @@ int main(void)
 
 	for (x = 48, n = 0; x <= 57; x++, n++)
 	{
-		for (y = 48 + n; y <= 57; y++)
-		{
-			if (x == y)
-			{
-				continue;
-			}
-			else if (x == 56 && y == 57)
-			{
-				putchar(x);
-				break;
-			}
-			else
-			{
-				putchar(x);
-				putchar(y);
-				putchar(44);
-				putchar(32);
-			}
-		}
+	for (y = 48 + n; y <= 57; y++)
+	{
+	if (x != y)
+	{
+	putchar(x);
+	putchar(y);
+	if (x == 56 && y == 57)
+	continue;
+	putchar(44);
+	putchar(32);
 	}
+	}
+	}
+	putchar(10);
 	return (0);
 }
