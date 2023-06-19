e1e12b54ffaedb33b9e7cb4066cb661197eac7a7
geraldrolland <geraldrolland123@gmail.com>
2023-06-19 01:42:06 -0700

    my task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 122; x >= 97; x--)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
