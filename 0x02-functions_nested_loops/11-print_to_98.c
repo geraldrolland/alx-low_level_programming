fbfb69d3ba078267d3b0f94e3f2db423ee6ea602
geraldrolland <geraldrolland123@gmail.com>
2023-06-21 08:56:46 -0700

    my task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,9 +12,13 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-	for (x = n; x <= 98; x++)
+	for (x = n; x < 98; x++)
 	{
 	printf("%d, ", x);
+	}
+	if (x == 98)
+	{
+	printf("%d", x);
 	}
 	}
 	else
