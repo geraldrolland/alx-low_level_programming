5226851b7f44b6a9d47e4d1f5a4e33705801f11c
geraldrolland <geraldrolland123@gmail.com>
2023-06-21 13:50:18 -0700

    my task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,50 +4,69 @@
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
+
 {
-	int count;
-	unsigned long fib1;
-	unsigned long fib2;
-	unsigned long sum;
-	unsigned long fib1_half1;
-	unsigned long fib1_half2;
-	unsigned long fib2_half1;
-	unsigned long fib2_half2;
-	unsigned long half1;
-	unsigned long half2;
 
+unsigned long int i;
 
-	fib1 = 0;
-	fib2 = 1;
-	for (count = 0; count < 92; count++)
-	{
-	sum = fib1 + fib2;
-	printf("%lu, ", sum);
-	fib1 = fib2;
-	fib2 = sum;
-	}
-	fib1_half1 = fib1 / 1000000000;
-	fib2_half1 = fib2 / 1000000000;
-	fib1_half2 = fib1 % 1000000000;
-	fib2_half2 = fib2 % 1000000000;
-	for (count = 93; count < 99; count++)
-	{
-	half1 = fib1_half1 + fib2_half1;
-	half2 = fib1_half2 + fib2_half2;
-	if (fib1_half2 + fib2_half2 > 9999999999)
-	{
-	half1 += 1;
- 	half2 %= 10000000000;
-	}
-	printf("%lu%lu", half1, half2);
-	if (count != 98)
-	printf(",");
-	fib1_half1 = fib2_half1;
-	fib1_half2 = fib2_half2;
-	fib2_half1 = half1;
-	fib2_half2 = half2;
-	}
-	printf("\n");
-	return (0);
+unsigned long int bef = 1;
+
+unsigned long int aft = 2;
+
+unsigned long int l = 1000000000;
+
+unsigned long int bef1;
+
+unsigned long int bef2;
+
+unsigned long int aft1;
+
+unsigned long int aft2;
+
+printf("%lu", bef);
+
+for (i = 1; i < 91; i++)
+
+{
+
+printf(", %lu", aft);
+
+aft += bef;
+
+bef = aft - bef;
+
+}
+
+bef1 = (bef / l);
+
+bef2 = (bef % l);
+
+aft1 = (aft / l);
+
+aft2 = (aft % l);
+
+for (i = 92; i < 99; ++i)
+
+{
+
+printf(", %lu", aft1 + (aft2 / l));
+
+printf("%lu", aft2 % l);
+
+aft1 = aft1 + bef1;
+
+bef1 = aft1 - bef1;
+
+aft2 = aft2 + bef2;
+
+bef2 = aft2 - bef2;
+
+}
+
+printf("\n");
+
+return (0);
+
 }
