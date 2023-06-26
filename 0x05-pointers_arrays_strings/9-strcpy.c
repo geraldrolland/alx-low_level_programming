239430c176bc76e99d22c192e91604c1ce57c4a4
geraldrolland <geraldrolland123@gmail.com>
2023-06-26 13:36:44 -0700

    my task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_strcpy - function that copies the string pointed to by src
+*@dest: check varaiable dest
+*@src: check source value
+* Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[1] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
+}
