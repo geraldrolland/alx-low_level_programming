41a2c86aa46c0deaa75ad5881c7074f2c1a0b50a
geraldrolland <geraldrolland123@gmail.com>
2023-06-26 12:36:34 -0700

    my task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*puts2 - Write a function that prints every other character of a string
+*@str: check variable str
+*Return: void
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar(10);
+}
