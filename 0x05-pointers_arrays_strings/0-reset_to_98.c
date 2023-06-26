5f0a0d8d16dc9743c98e9c8fc0862916db1b08ec
geraldrolland <geraldrolland123@gmail.com>
2023-06-26 01:46:09 -0700

    my task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - as parameter and updates the value it points to to 98
+*@n:chech character n
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
