46228390ec51d4b18f79f18c44b305f612954fec
geraldrolland <geraldrolland123@gmail.com>
2023-06-27 11:32:37 -0700

    my task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,16 +9,25 @@
 
 int main(void)
 {
-	int i;
-	char passwd;
+	int pass[100];
+	int i, sum, n;
 
-	printf("Password: ");
+	sum = 0;
+
 	srand(time(NULL));
-	for (i = 0; i < 12; i++)
+
+	for (i = 0; i < 100; i++)
 	{
-		passwd = (char) ((rand() % 55) + 48);
-		printf("%c", passwd);
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	printf("\n");
 	return (0);
 }
