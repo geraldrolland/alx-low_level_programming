e4a12b6d35bd4cf2c17b18f9ba07712ca8ae225b
geraldrolland <geraldrolland123@gmail.com>
2023-07-03 05:49:05 -0700

    my task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_memset - Write a function that fills memory with a constant byte.
+*@s: input value;
+*@b:input value
+*@n: input value
+*Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	*s = b;
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
