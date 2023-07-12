7b998e3239c221d2d2cada73caced25ed1392b59
geraldrolland <geraldrolland123@gmail.com>
2023-07-12 12:21:36 -0700

    my task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_strdup -  which contains a copy of the string given as a parameter.
+*@str: input value
+*Return: NULL or ptr
+*/
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, size, count;
+
+	count = 0;
+	for (i = 0; *(str + i) != '\0' ; i++)
+	{
+		count = count + 1;
+
+	}
+	size = count;
+	ptr = (char *)malloc(size * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		*(ptr + i) = *(str + i);
+	}
+	*(ptr + i) = '\0';
+	return (ptr);
+}
