360830ed8aedfd629c5fcd4d55b5f7caa4fd0d3f
geraldrolland <geraldrolland123@gmail.com>
2023-07-14 12:13:52 -0700

    my task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
 *string_nconcat - Write a function that concatenates two strings.
 *@s1: input value
@@ -10,39 +9,36 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, size, k;
+	unsigned int i = 0, j = 0, l = 0, k = 0;
 	char *ptr1;
 
-	if (n >= strlen(s2))
-	{
-	size = strlen(s1) + strlen(s2) + 1;
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[k])
+		k++;
+	if (n >= k)
+		l = i + k;
 	else
+		l = i + n;
+	ptr1 = malloc(sizeof(char) * l + 1);
+	if (ptr1 == NULL)
+		return (NULL);
+	k = 0;
+	while (j < l)
 	{
-	size = strlen(s1) + n + 1;
+		if (j <= i)
+			ptr1[j] = s1[j];
+		if (j >= i)
+		{
+			ptr1[j] = s2[k];
+			k++;
+		}
+		j++;
 	}
-	ptr1 = (char *)malloc(size * sizeof(char));
-	for (i = 0, j = 0; i < strlen(s1) + 1; j++, i++)
-	{
-	if (*(s1 + i) == '\0')
-	{
-	if (n >= strlen(s2))
-	{
-	for (k = 0; *(s2 + k) != 0; k++, j++)
-	{
-	*(ptr1 + j) = *(s2 + k);
-	}
-	}
-	else
-	{
-	for (k = 0; k < n; k++, j++)
-	{
-	*(ptr1 + j) = *(s2 + k);
-	}
-	}
-	}
-	*(ptr1 + j) = *(s1 + i);
-	}
-	*(ptr1 + j) = '\0';
+	ptr1[j] = '\0';
 	return (ptr1);
 }
