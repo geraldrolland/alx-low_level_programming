4a2351a7be04f4f11189cb76710f6d822ac7df9e
geraldrolland <geraldrolland123@gmail.com>
2023-07-17 19:33:10 -0700

    my task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - declaring a structure of tag dog
+*@name: member
+*@age: member
+*@owner: member
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
