b6d3462fda66fe3b706500e64ffbaca36a17776f
geraldrolland <geraldrolland123@gmail.com>
2023-07-18 11:24:03 -0700

    my task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,17 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdlib.h>
+#include "dog.h"
 /**
-*struct dog - declaring a structure of tag dog
-*@name: member
-*@age: member
-*@owner: member
+*init_dog - initialize a variable of type struct dog
+*@d: pointer to struct dog to initialize
+*@name: name to initialize
+*@age: age to initailise
+*@owner: owner to initialise
 */
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-#endif
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
