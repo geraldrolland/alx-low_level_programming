f6dbacf3d810f6c0dcb1bca86dafc66e8bba310b
geraldrolland <geraldrolland123@gmail.com>
2023-07-18 11:31:43 -0700

    my task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+*struct dog - initialise structure
+*@name : structure member
+*@age: structure member
+*@owner: structure member
+*/
 struct dog
 {
 	char *name;
