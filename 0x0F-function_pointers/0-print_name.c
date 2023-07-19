622b974af5bb56170c3793b8a9627e2d193ae21d
geraldrolland <geraldrolland123@gmail.com>
2023-07-19 10:24:37 -0700

    my task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+#include <stdio.h>
 /**
 *print_name - print a name
 *@name: pointer to a string
@@ -6,5 +8,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	(*f)(name);
 }
