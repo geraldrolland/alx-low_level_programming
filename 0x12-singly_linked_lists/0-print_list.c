3e2ee523aa147d8229c14fd639387d35ab44525b
geraldrolland <geraldrolland123@gmail.com>
2023-07-28 03:50:54 -0700

    mybtask 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+*print_list - prints all the elements of a list_t list.
+*@h: pointer to a constant string
+*Return: count or NULL
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+	int i;
+
+	i = 0;
+	count = 0;
+	while(1)
+	{
+	if (h==NULL)
+	{
+		return count;
+        }
+		count++;
+		if (h->str==NULL)
+		{
+			printf("[0] (nil)");
+			printf("\n");
+		}
+		printf("[%u] %s", h->len, h->str);
+		printf("\n");
+		h=h->next;
+		i++;
+	}
+	return count;
+}
---
+++ 0x12-singly_linked_lists/_strlen.c
@@ -0,0 +1,17 @@
+/**
+*_strlen - prints the lenght of a string
+*@str: pointer to a constant string
+*Return: size
+*/
+int _strlen(const char *str)
+{
+	int size;
+	int i;
+
+	size = 0;
+	for(i = 0; *(str+i) != '\0'; i++)
+	{
+		size++;
+	}
+	return (size);
+}
