f7d28bbf69c92001a1c98b594b802cf094cfddb7
geraldrolland <geraldrolland123@gmail.com>
2023-07-28 11:21:21 -0700

    my task 0 updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,22 +8,6 @@
 
 size_t print_list(const list_t *h)
 {
-	/*size_t count;
-
-	count = 0;
-	while (h)
-	{
-	if (!h->str)
-	{
-	printf("[0] (nil)");
-	printf("\n");
-	}
-	printf("[%u] %s", h->len, h->str);
-	printf("\n");
-	h = h->next;
-	count++;
-	}
-	return (count);*/
 	size_t count = 0;
 
 	while (h)
