6bdd6a07884a4a7aefb5e1e7e4646bb373b9804d
geraldrolland <geraldrolland123@gmail.com>
2023-08-01 04:42:30 -0700

    my task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,15 +2,15 @@
 /**
 *free_listint2 - free the list
 *@head: pointer to the head pointer
-*Return; void
+*Return: void
 */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 	listint_t *ptr = *head;
 
-	if (*head == NULL)
-		return (NULL);
+	/*if (head == NULL)
+		return (NULL);*/
 
 	while (1 > 0)
 	{
