3efc1d300fc7c7cb36517a45dd0b0e014a6d63d9
geraldrolland <geraldrolland123@gmail.com>
2023-08-01 12:07:07 -0700

    my task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL || head == NULL)
+	if (!new || !head)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
@@ -29,6 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (j == idx - 1)
 		{
 			new->next = ptr->next;
+			ptr->next = new;
 			return (new);
 		}
 		ptr = ptr->next;
