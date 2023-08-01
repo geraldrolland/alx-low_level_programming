186a41ccf94f1f13eb45417b4d0a04bf125b5df7
geraldrolland <geraldrolland123@gmail.com>
2023-08-01 04:31:37 -0700

    my task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+*free_listint2 - free the list
+*@head: pointer to the head pointer
+*Return; void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *ptr = *head;
+
+	while (1 > 0)
+	{
+		if (ptr->next == NULL)
+		{
+			free(*head);
+			*head = NULL;
+			break;
+		}
+		temp = ptr;
+		free(temp);
+		ptr = ptr->next;
+		*head = ptr;
+	}
+}
