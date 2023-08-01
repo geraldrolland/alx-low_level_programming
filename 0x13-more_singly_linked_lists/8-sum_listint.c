35dfb9fef06214ab7f732ac473c84795fe80bbfd
geraldrolland <geraldrolland123@gmail.com>
2023-08-01 11:41:42 -0700

    my task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+*sum_listint - return the sum of element in a list
+*@head: pointer to the first node
+*Return: sum
+*/
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr = head;
+	int sum = 0;
+
+	while (1 > 0)
+	{
+		if (ptr->next == NULL)
+		{
+			sum = sum + (ptr->n);
+			break;
+		}
+		sum = sum + (ptr->n);
+		ptr = ptr->next;
+	}
+	return (sum);
+}
