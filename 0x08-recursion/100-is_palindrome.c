aff51fe26aa74a5f85d58c70184743f07a96e0f3
geraldrolland <geraldrolland123@gmail.com>
2023-08-30 05:45:45 -0700

    my task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *is_palindrome - returns 1 if a string is palindrome and 0 if not
+ *@s: pointer to a string
+ *Return: res on success
+ */
+int is_palindrome(char *s)
+{
+	int res;
+	char ch = *s;
+
+	res = check_palin(s, ch);
+
+	return (res);
+}
+/**
+ *check_palin - check if a number is a palindrome
+ *@s: pointer to a string
+ *@ch: character variable
+ */
+int check_palin(char *s, char ch)
+{
+	int num;
+
+	s = s + 1;
+	if (*s == ch && *(s + 1) == '\0')
+		return (1);
+	if (*s != ch && *(s + 1) =='\0')
+		return (0);
+	num = check_palin(s, ch);
+	return (num);
+}
