cc8465a6d5bd273576ae52ab656c1093d2f673e4
geraldrolland <geraldrolland123@gmail.com>
2023-08-31 07:23:50 -0700

    my task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,18 +7,24 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	int res;
-
-	if (*s1==*s2 && *(s1+1)=='\0' && *(s2+1)=='\0'){
-		return 1;
+	if (*s1 == '\0')
+	{
+	if (*s2 != '\0' && *s2 == '*')
+	{
+	s2 = s2 + 1;
+	return (wildcmp(s1, s2));
 	}
-	if (*s1==*s2){
-		s1=s1+1;
-		s2=s2+1;
-		res = wildcmp(s1,s2);
-		return res;
+	return (*s2 == '\0');
 	}
-	else{
-		return 0;
+	if (*s2 == '*')
+	{
+	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
+	else if (*s1 == *s2)
+	{
+	s2 = s2 + 1;
+	s1 = s1 + 1;
+	return (wildcmp(s1, s2));
+	}
+	return (0);
 }
