6f917da1d029c205b43fcc0d8fb74431a09c9578
geraldrolland <geraldrolland123@gmail.com>
2023-09-02 12:27:27 -0700

    my task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) == 0 || atoi(agrv[2]))
+	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
 	{
 		printf("Error\n");
 		return (1);
