1462184e259c7313447491500f56a352ce90ddc6
geraldrolland <geraldrolland123@gmail.com>
2023-09-04 11:52:36 -0700

    my task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_strdup - return copy of string
+ *@str: pointer to a string
+ *Return: ptr on success or NULL on failure
+ */
+char *_strdup(char *str)
+{
+	int k, i;
+	int size = 0;
+	char *ptr = (char *)malloc(sizeof(str));
+
+	if (ptr == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; *(str + k) != '\0'; k++)
+	{
+		size++;
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(ptr + i) = *(str + i);
+	}
+	*(ptr + i) = '\0';
+	return (ptr);
+}
