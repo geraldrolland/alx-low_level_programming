b32ac7fe2ae867d1dd476881b34890cf88da5497
geraldrolland <geraldrolland123@gmail.com>
2023-09-07 13:41:51 -0700

    my task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *ptr1;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	ptr1 = malloc((nmemb) * size);
 	if (ptr1 == NULL)
