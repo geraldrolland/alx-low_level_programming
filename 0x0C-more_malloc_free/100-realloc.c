7558de6a1308526400b5921739d0fa20d1c90cd4
geraldrolland <geraldrolland123@gmail.com>
2023-09-07 13:09:04 -0700

    my task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,10 +38,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_ptr[i] = (*(char *)ptr + i);
 		}
 	}
+	if (ptr != NULL && new_size < old_size)
+	{
 	new_ptr = malloc(new_size);
 	for (i = 0; i < new_size + 1; i++)
 	{
 		new_ptr[i] = (*(char *) ptr + i);
+	}
 	}
 	free(ptr);
 	return (new_ptr);
