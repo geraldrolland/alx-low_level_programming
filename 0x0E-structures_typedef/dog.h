480ab43528a03d3567efe4bde3c647125262034e
geraldrolland <geraldrolland123@gmail.com>
2023-09-11 01:17:25 -0700

    my task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - user defined datatype
+ *@name: string member
+ *@age: float member
+ *@owner: string member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
