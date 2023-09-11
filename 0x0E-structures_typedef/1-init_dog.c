1f4f755fb691edf5fb372ff4cac8c3cad5a1e5c7
geraldrolland <geraldrolland123@gmail.com>
2023-09-11 09:53:07 -0700

    my task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,9 +9,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (name == NULL)
-		d->name = "(nil)";
+		name = "(nil)";
 	if (owner == NULL)
-		d->owner = "(nil)";
+		owner = "(nil)";
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
