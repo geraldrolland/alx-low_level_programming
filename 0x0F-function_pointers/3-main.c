c1596b6d34ee558a0e8377fe1ced5aae3fc8696c
geraldrolland <geraldrolland123@gmail.com>
2023-09-13 14:23:14 -0700

    my task all

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ *main - main function
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int calc, num1, num2;
+	int (*ptr)(int , int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+			argv[2][0] != '/' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((argv[2][0] == '/' || argv[2][0] == '%')
+			&& num2 == 0 && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	ptr = get_op_func(argv[2]);
+	calc = ptr(num1, num2);
+	printf("%d\n", calc);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ *op_add - return sum of a and b
+ *@a: integer a;
+ *@b: interger b
+ *Return: a+b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - return difference of a and b
+ *@a: integer variable
+ *@b: integer variable
+ *Return: a * b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - product of a and b
+ *@a: integer variable
+ *@b: integer variable
+ *Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divide a with b
+ *@a: integer variable
+ *@b: integer variable
+ *Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - return the remainder of a in mod b
+ *@a: integer variable
+ *@b: integer variable
+ *Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
