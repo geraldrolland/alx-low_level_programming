8f51df0e68813bda2db8e94a0583db8a496d7a37
geraldrolland <geraldrolland123@gmail.com>
2023-09-14 08:24:04 -0700

    my task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ *sum_them_all - sum all number argument parsed
+ *@n: number of argument
+ *Return: sum on success or NULL on failure
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}
