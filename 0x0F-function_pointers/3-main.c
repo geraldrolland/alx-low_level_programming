ade574351e19bc3f5a4856f3f8568d19ddafbbea
geraldrolland <geraldrolland123@gmail.com>
2023-09-14 02:19:29 -0700

    my task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,28 +11,28 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-	int (*ptr)(int, int);
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
-			argv[2][0] != '/' && argv[2][0] != '%')
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+			argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	op = argv[2];
 	if ((argv[2][0] == '/' || argv[2][0] == '%')
 			&& num2 == 0 && argv[3][0] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	ptr = get_op_func(argv[2]);
-	printf("%d\n", ptr(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -12,3 +12,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 #endif
+
