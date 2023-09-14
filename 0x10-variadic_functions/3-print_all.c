57fb22baf885c7b96cf6f38d2650e972c95478f1
geraldrolland <geraldrolland123@gmail.com>
2023-09-14 13:48:12 -0700

    my task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,31 +11,38 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	int int_num;
 	char ch;
+	float float_num;
 	char *string;
 
 	va_start(list, format);
 	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] == 'i')
+		switch (format[i])
 		{
-			int_num = va_arg(list, int);
-			printf("%d", int_num);
+			case 'c':
+				ch = va_arg(list, int);
+				printf("%c", ch);
+				break;
+			case 's':
+				string = va_arg(list, char *);
+				if (string == NULL)
+					string = "(nil)";
+				printf("%s", string);
+				break;
+			case 'f':
+				float_num = va_arg(list, double);
+				printf("%f", float_num);
+				break;
+			case 'i':
+				int_num = va_arg(list, int);
+				printf("%d", int_num);
+				break;
+			default:
+				i++;
+				continue;
 		}
-		else if (format[i] == 'c')
-		{
-			ch = va_arg(list, int);
-			printf("%c", ch);
-		}
-		else if (format[i] == 's')
-		{
-			string = va_arg(list, char *);
-			printf("%s", string);
-		}
-		if (format[i + 1] != '\0' &&
-				(format[i] == 'i' || format[i] == 'c' || format[i] == 's'))
-		{
+		if (format[i + 1] != '\0')
 			printf(", ");
-		}
 		i++;
 	}
 	va_end(list);
