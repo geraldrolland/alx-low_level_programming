12c1473caa87fb9f6e354b49fc273548620c918e
geraldrolland <geraldrolland123@gmail.com>
2023-09-21 11:32:51 -0700

    my task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *add_node_end - add node at the beginning of the list
+ *@head: head to the pointer of the first node
+ *@str: string to be copied
+ *Return: *head on succes or NULL on failure
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr = *head;
+	list_t *new;
+	unsigned int i, len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	for (i = 0; new->str[i]; i++)
+	{
+		len++;
+	}
+	new->len = len;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	while (1 > 0)
+	{
+		if (ptr->next == NULL)
+		{
+			ptr->next = new;
+			break;
+		}
+		ptr = ptr->next;
+	}
+	return (*head);
+}
