eeb150d3f36cfac1bf302f6371f02ea8c00e395e
geraldrolland <geraldrolland123@gmail.com>
2023-09-22 13:35:57 -0700

    my task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,6 +18,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	for (i = 0; new->str[i]; i++)
 	{
 		len++;
