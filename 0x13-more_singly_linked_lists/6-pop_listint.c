03db3da27f65ffb0acb4f3fd9358e3bbaad54dff
geraldrolland <geraldrolland123@gmail.com>
2023-09-26 08:55:55 -0700

    my task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,9 +10,9 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int data;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL || *head == NULL)
 	{
-		;
+		return 0;
 	}
 
 	temp = *head;
