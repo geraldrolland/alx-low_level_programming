bf9b400fcc2117025184df8b077969c859ecc989
geraldrolland <geraldrolland123@gmail.com>
2023-10-02 15:53:49 -0700

    my task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,6 @@ int create_file(const char *filename, char *text_content)
 		if (fd == -1)
 			return (-1);
 		close(fd);
-		return (-1);
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#include <stdlib.h>
+int append_text_to_file(const char *filename, char *text_content)
+{
+	 
+}
