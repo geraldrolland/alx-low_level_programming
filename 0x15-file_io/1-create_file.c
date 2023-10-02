40d74160b1432750234fef66e2410ed69b942031
geraldrolland <geraldrolland123@gmail.com>
2023-10-02 15:19:33 -0700

    my task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "main.h"
+size_t _strlen(char *str);
+/**
+ *create_file - creates a file.
+ *@filename: name of file
+ *@text_content: text content
+ *Return: 1 on success or -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, buf;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT | O_RDWR);
+		if (fd == -1)
+			return (-1);
+		close(fd);
+		return (1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_EXCL | O_TRUNC);
+	if (fd == -1)
+		return (-1);
+	buf = write(fd, text_content, _strlen(text_content));
+	if (buf == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
+/**
+ *_strlen - return the lenght of string
+ *@str: pointer to a string
+ *Return: count
+ */
+size_t _strlen(char *str)
+{
+	int i;
+	size_t count = 0;
+	for (i = 0; str[i]!='\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
