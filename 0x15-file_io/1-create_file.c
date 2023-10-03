fffc51474a73abffb518f20b9adacce71ff4813a
geraldrolland <geraldrolland123@gmail.com>
2023-10-02 17:02:17 -0700

    my task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@ size_t _strlen(char *str);
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fd, buf;
+	ssize_t fd, buf = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,6 +20,8 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		close(fd);
 	}
+	else
+	{
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
@@ -29,6 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd);
+	}
 	return (1);
 }
 /**
