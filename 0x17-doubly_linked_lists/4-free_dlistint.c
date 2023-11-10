0b1634c7ead3bbc99720bdb06984fc8a6d137f91
geraldrolland <geraldrolland123@gmail.com>
2023-11-10 02:47:27 -0800

    my task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 
 /**
- *free_dlistint - free a doubly linked list
- *@head: pointer to the head node
- *Return: head on sucess or NULL on failure
+ * free_dlistint - free a doubly linked list
+ * @head: pointer to the head node
+ * Return: void
  */
 void free_dlistint(dlistint_t *head)
 {
-	if (head == NULL)
-		return (NULL);
-	head = head->next;
-	free(head->prev);
+	dlistint_t *temp;
+
 	while (head != NULL)
 	{
+		temp = head;
 		head = head->next;
-		free(head->prev);
-
+		free(temp);
 	}
-	return (head);
 }
+
