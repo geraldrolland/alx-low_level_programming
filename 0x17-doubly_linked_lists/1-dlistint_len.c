4f3c8cd9a14f710a75392936424577c46d4069df
geraldrolland <geraldrolland123@gmail.com>
2023-11-10 00:35:35 -0800

    my task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *dlistint_len - return the size of a list
+ *@h: pointer to the head node
+ *Return: count
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	dlistint_t *ptr = h;
+
+	if (h == NULL)
+		return (0);
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
