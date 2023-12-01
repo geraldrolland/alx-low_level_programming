069740462fa02027140e81f4bb64e8e903fd8857
geraldrolland <geraldrolland123@gmail.com>
2023-12-01 12:48:56 -0800

    my task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_get - get the value at a specified key
+ *@ht: hash table
+ *@key: key
+ *Return: value on success
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int hash_code;
+	hash_node_t *ptr;
+
+	if (strlen(key) == 0)
+		return NULL;
+	hash_code = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[hash_code];
+	if (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+			return (ptr->value);
+		ptr = ptr->next;
+		while (ptr != NULL)
+		{
+			if(strcmp(ptr->key, key) == 0)
+				return (ptr->value);
+			ptr = ptr->next;
+		}
+		return (NULL);
+	}
+	return (NULL);
+}
