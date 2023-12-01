e7ed03aa60d7344a3539711c76a8c0bcf5931d38
geraldrolland <geraldrolland123@gmail.com>
2023-12-01 04:22:40 -0800

    my task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (my_table == NULL)
 		return (NULL);
 	my_table->size = size;
-	my_table->array = malloc(sizeof(hash_node_t *) * size);
+	my_table->array = malloc(sizeof(hash_node_t *) * size + 1);
 	if (my_table->array == NULL)
 		return (NULL);
 	return (my_table);
