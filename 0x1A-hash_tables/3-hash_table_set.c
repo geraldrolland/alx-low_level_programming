67247b77eb3f4b6e291001010f6e16fa8d9c0ffa
geraldrolland <geraldrolland123@gmail.com>
2023-12-01 09:54:41 -0800

    my task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+int check_dup(hash_node_t *ptr, const char *key, const char *value);
 
 /**
  * hash_table_set - insert an element in the hash table
@@ -10,44 +11,84 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *key_copy;
-	char *value_copy;
+	char *key_copy, *value_copy;
 	unsigned long int hash_code;
-	hash_node_t *new_node;
-	hash_node_t *ptr;
+	hash_node_t *new_node, *ptr;
 
 	if (key == NULL || strlen(key) == 0 || value == NULL || ht == NULL)
 		return (0);
-	hash_code = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	key_copy = strdup(key);
-	value_copy = strdup(value);
-	if (key_copy == NULL || value_copy == NULL)
-		return (0);
+	hash_code = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[hash_code] != NULL)
 	{
 		ptr = ht->array[hash_code];
-		while (ptr != NULL)
-		{
-			if (strcmp(ptr->key, key_copy) == 0)
-			{
-				free(ptr->value);
-				free(key_copy);
-				ptr->value = value_copy;
-				return (1);
-			}
-			ptr = ptr->next;
-		}
-		ptr = ht->array[hash_code];
-		new_node->next = ptr;
-		new_node->key = key_copy;
-		new_node->value = value_copy;
-		ht->array[hash_code] = new_node;
-		return (1);
+		free(new_node);
+		return (check_dup(ptr, key, value));
 	}
+	key_copy = strdup(key);
+	if (key_copy == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+	{
+		free(new_node);
+		free(key_copy);
+		return (0);
+	}
+	new_node->key = key_copy;
+	new_node->value = value_copy;
+	new_node->next = NULL;
 	ht->array[hash_code] = new_node;
 	return (1);
+}
 
+/**
+ *check_dup - check for duplicate element
+ *@ptr: pointer to the first node in the list
+ *@value: value
+ *@key: key
+ *Return: 1
+ */
+int check_dup(hash_node_t *ptr, const char *key, const char *value)
+{
+	hash_node_t *temp;
+	char *value_copy, *key_copy;
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+	temp = ptr;
+	while (ptr != NULL)
+	{
+		if (strcmp(ptr->key, key) == 0)
+		{
+			free(ptr->value);
+			ptr->value = value_copy;
+			return (1);
+		}
+		ptr = ptr->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	key_copy = strdup(key);
+	if (key_copy == NULL)
+	{
+		free(new_node);
+		free(value_copy);
+		return (0);
+	}
+	new_node->next = temp;
+	new_node->key = key_copy;
+	new_node->value = value_copy;
+	temp = new_node;
+	return (1);
 }
