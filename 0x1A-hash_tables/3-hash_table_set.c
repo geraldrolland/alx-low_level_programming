6dd416defe6a953c460e0760f2a9ce8b68e13e36
geraldrolland <geraldrolland123@gmail.com>
2023-12-01 08:21:56 -0800

    my task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,11 +12,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	char *key_copy;
 	char *value_copy;
-	unsigned long int hash_code;
+	unsigned long int hash_code, i;
 	hash_node_t *new_node;
 	hash_node_t *ptr;
 
-	if (key == NULL || strlen(key) == 0 || value == NULL)
+	if (key == NULL || strlen(key) == 0 || value == NULL || ht == NULL)
 		return (0);
 	key_copy = strdup(key);
 	value_copy = strdup(value);
@@ -30,13 +30,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(value_copy);
 		return (0);
 	}
-	new_node->key = key_copy;
-	new_node->value = value_copy;
-	new_node->next = NULL;
 	if (ht->array[hash_code] != NULL)
 	{
 		ptr = ht->array[hash_code];
+		while(ptr != NULL)
+		{
+			if (strcmp(ptr->key, key_copy) == 0)
+			{
+				free(ptr->value);
+				free(key_copy);
+				ptr->value = value_copy;
+				return (1);
+			}
+			ptr = ptr->next;
+		}
+		ptr = ht->array[hash_code];
 		new_node->next = ptr;
+		new_node->key = key_copy;
+		new_node->value = value_copy;
 		ht->array[hash_code] = new_node;
 		return (1);
 	}
