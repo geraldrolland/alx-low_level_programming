84de677a74d995b8cc4282198b1ca7979ea38500
geraldrolland <geraldrolland123@gmail.com>
2024-01-25 01:04:23 -0800

    Mytask

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""
+This module defines the function island_perimeter()
+"""
+
+
 def island_perimeter(grid):
+    """This function return the perimeter of the island
+    args:
+        grid(list): list of list as grid
+    """
+    num_row = 0
+    for row in grid:
+        num_row += 1
+    num_column = len(grid[0])
+    return (num_row - 2) * (num_column - 2)
     
